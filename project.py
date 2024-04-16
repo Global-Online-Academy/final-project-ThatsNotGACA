56e4f9372ca282a29eab8eeb63d17667348d1d18
Ethan Hoffman <ethanhoffman@Ethan-MacbookAir-New.local>
2024-04-16 12:11:46 -0700

    Final Project

--- project.py
+++ project.py
@@ -1,16 +1,17 @@
 from bokeh.plotting import figure, show
 from bokeh.io import output_file
 from bokeh.models import BasicTickFormatter
+from bokeh.layouts import gridplot
 
-with open('data.txt', 'r') as file:
-    data = file.readlines()
+with open('data1.txt', 'r') as file:
+    data1 = file.readlines()
 
 years = []
 col2 = []
 col4 = []
 col5 = []
 col7 = []
-for line in data:
+for line in data1:
     line_data = line.strip().split('\t')
     if len(line_data) >= 5:
         year = line_data[0].split('-')[0]
@@ -20,7 +21,6 @@ for line in data:
         col5.append(float(line_data[4].replace(',', ''))) if line_data[4] else col5.append(0)
         col7.append(float(line_data[6].replace(',', ''))) if line_data[6] else col7.append(0)
 
-output_file("plot.html")
 
 p = figure(title="Number of Computer Science Degrees Per Year", x_axis_label='Year', y_axis_label='Number of Degrees')
 
@@ -31,4 +31,74 @@ p.line(years, col7, legend_label="Total Master Degrees", line_width=2, line_colo
 
 p.yaxis.formatter = BasicTickFormatter(use_scientific=False)
 
-show(p)
+years2 = []
+jobs = []
+pay = []
+
+with open('data2.txt', 'r') as file:
+    data2 = file.readlines()
+
+for line in data2:
+    line_data = line.strip().split('    ')
+    if len(line_data) >= 3:
+        year = line_data[0].split('-')[0]
+        years2.append(year)
+        jobs.append(float(line_data[1].replace(',', '')))
+        pay_value = line_data[2].replace(',', '')
+        pay.append(float(pay_value))
+
+
+
+with open('data3.txt', 'r') as file:
+    data3 = file.readlines()
+
+years3 = []
+jobs2 = []
+pay2 = []
+
+for line in data3:
+    line_data = line.strip().split('    ')
+    if len(line_data) >= 3:
+        year = line_data[0].split('-')[0]
+        years3.append(year)
+        jobs2.append(float(line_data[1].replace(',', '')))
+        pay2.append(float(line_data[2].replace(',', '')))
+
+with open('data4.txt', 'r') as file:
+    data4 = file.readlines()
+
+years4 = []
+pay3 = []
+
+for line in data4:
+    line_data = line.strip().split()
+    if len(line_data) >= 2:
+        year = line_data[0]
+        years4.append(year)
+        cost = float(line_data[1].replace(',', ''))
+        pay3.append(cost)
+
+t = figure(title="Number of Computer Jobs Per Year", x_axis_label='Year', y_axis_label='Number of Jobs')
+u = figure(title="Annual Income of Computer Jobs Per Year", x_axis_label='Year', y_axis_label='Annual Income')
+t.line(years3, jobs2, legend_label="Number of Software Development Jobs", line_width=2, line_color="red")
+t.line(years2, jobs, legend_label="Number of Computer Science Jobs", line_width=2, line_color="blue")
+u.line(years3, pay2, legend_label="Annual Pay of Software Development", line_width=2, line_color="red")
+u.line(years2, pay, legend_label="Annual Pay of Computer Sceince", line_width=2, line_color="green")
+u.line(years4, pay3, legend_label="Annual Pay of Other Jobs", line_width=2, line_color="blue")
+
+t.yaxis.formatter = BasicTickFormatter(use_scientific=False)
+u.yaxis.formatter = BasicTickFormatter(use_scientific=False)
+
+
+output_file("plot.html")
+
+grid = gridplot([[p, None], [t, u]])
+
+show(grid)
+
+
+
+#Sources:
+#https://nces.ed.gov/programs/digest/d22/tables/dt22_325.35.asp
+#https://codesubmit.io/blog/the-evolution-of-developer-salaries/
+#https://www.ssa.gov/oact/cola/awidevelop.html
---
+++ projectREADME.md
@@ -0,0 +1,7 @@
+# Final Project: How has the Demand for Computer Science Jobs Changed in the Last 50 Years?
+
+I answered this question by finding the change in various computer science job fields, their income, and the amount of bachelor and master degress handed out every year.
+
+With this data, I was able to draw several conclusions about the computer science industry and can predict what the future of the industry will entail.
+
+Please draw your own conclusions as well of what this data may mean and what the future of computer science will be.
